9b915c72a3b6e13e7613527d3bd808f5c4cf6f25
Ayoub Gutin <gutinayoub@gmail.com>
2026-08-02 22:53:24 +0100

    completed day four solution. Also made some helper funcs to help with deduplication. Might consider custom decorators for the duplicate loops in part one and two, as the only differientiator is modification of the element in place to `.`. Will have a think about it

--- advent_of_code/day_four/main.py
+++ advent_of_code/day_four/main.py
@@ -7,40 +7,103 @@ else, if < 4, then it is True, and we can add one to the counter.
 import operator
 from pathlib import Path
 
-# test with a sample arr
-arr = []
-path = Path('advent_of_code/day_four/input')
-with path.open('r', encoding='utf-8') as f:
-    for line in f:
-        line = line.strip()
-        line = list(line)
-        arr.append(line)
+DIRECTIONS: tuple[tuple[int, int]] = (
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
+    (1, 1),
+    (1, -1),
+    (-1, 1),
+    (-1, -1),
+)
 
 
-rows = len(arr)
-counter = 0
-directions = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
+def parse_input(filepath: str | Path):
+    arr = []
+    if isinstance(filepath, str):
+        filepath = Path(filepath)
+    with filepath.open('r', encoding='utf-8') as f:
+        for line in f:
+            arr.append(list(line.strip()))
 
-for row_idx, row in enumerate(arr):
-    bottom_idx = row_idx + 1 if row_idx != rows - 1 else -1
-    top_idx = row_idx - 1 if row_idx != 0 else -1
-    col_len = len(row)
+    return arr
 
-    for col_idx, col in enumerate(row):
-        sub_counter = 0
-        if col != '@':
-            continue
 
-        curr_pos = (row_idx, col_idx)
+def valid_roll(
+    arr: list[list[str]], curr_pos: tuple[int], dir: tuple[int], col_len: int
+):
+    check_idx = tuple(map(operator.add, curr_pos, dir))
+    if -1 in check_idx or col_len in check_idx:
+        return False
+    else:
+        return True if '@' in arr[check_idx[0]][check_idx[1]] else 0
 
-        for dir in directions:
-            check_idx = tuple(map(operator.add, curr_pos, dir))
-            if -1 in check_idx or col_len in check_idx:
+
+def part_one(filepath: str | Path):
+    arr = parse_input(filepath)
+    counter = 0
+    for row_idx, row in enumerate(arr):
+        col_len = len(row)
+
+        for col_idx, col in enumerate(row):
+            sub_counter = 0
+            if col != '@':
                 continue
-            else:
-                sub_counter += 1 if '@' in arr[check_idx[0]][check_idx[1]] else 0
 
-        if sub_counter < 4:
-            counter += 1
+            curr_pos = (row_idx, col_idx)
 
-print(counter)
+            for dir in DIRECTIONS:
+                sub_counter += (
+                    1 if valid_roll(arr, curr_pos, dir, col_len) == True else 0
+                )
+
+            if sub_counter < 4:
+                counter += 1
+
+    return counter
+
+
+def part_two(filepath: str | int):
+    """
+    Once a roll of paper can be accessed (< 4), it can be removed.
+    The forklift will repeat the process
+    """
+    arr = parse_input(filepath)
+    total_removed = 0
+
+    def pass_once():
+        changes = []
+        for row_idx, row in enumerate(arr):
+            col_len = len(row)
+
+            for col_idx, col in enumerate(row):
+                sub_counter = 0
+                if col != '@':
+                    continue
+
+                curr_pos = (row_idx, col_idx)
+
+                sub_counter = sum(
+                    1 for dir in DIRECTIONS if valid_roll(arr, curr_pos, dir, col_len)
+                )
+
+                if sub_counter < 4:
+                    changes.append(curr_pos)
+
+        for row, column in changes:
+            arr[row][column] = '.'
+
+        return len(changes)
+
+    while True:
+        removed_count = pass_once()
+        if removed_count == 0:
+            break
+        total_removed += removed_count
+
+    return total_removed
+
+
+part_two_res = part_two('advent_of_code/day_four/input')
+print(part_two_res)
