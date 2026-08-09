e47e7bd25becc74a872ddfe43f643447d17d4829
Ayoub Gutin <gutinayoub@gmail.com>
2026-08-09 23:49:34 +0100

    day 6 working, currently debugging

--- advent_of_code/day_six/main.py
+++ advent_of_code/day_six/main.py
@@ -1,14 +1,17 @@
-from pathlib import Path
 import operator
+from pathlib import Path
 from functools import reduce
+from itertools import zip_longest
+
 
 OPERATORS = {'+': operator.add, '*': operator.mul}
 
 
-def parse_input(filepath: str | Path):
+def parse_input_p1(filepath: str | Path):
     """
-    1. reads the entire file as one string. Then, removes trailing spaces at the beginning or end of the file, and then splits each line (\n)
-    2.
+    1. reads the entire file as one string, splits each line afterwards (\n)
+    2. parses the operators as one list.
+    3. takes the rest of the lines and turns it into a nested lsit of ints, representing ther lines.
     """
 
     lines = Path(filepath).read_text(encoding='utf-8').splitlines()
@@ -22,8 +25,8 @@ def parse_input(filepath: str | Path):
     return ops, columns
 
 
-def grand_total(filepath: str | Path):
-    ops, columns = parse_input(filepath)
+def grand_total_p1(filepath: str | Path):
+    ops, columns = parse_input_p1(filepath)
 
     total = 0
     for op, numbers in zip(ops, columns):
@@ -33,4 +36,29 @@ def grand_total(filepath: str | Path):
     return total
 
 
-print(grand_total('advent_of_code/day_six/input.txt'))
+def grand_total_p2(filepath: str | Path):
+    """
+    Similar to p1, but instead of taking the number as the field, a number is composed of reading it from top to bottom in a column
+    Most significant digit is at the first row, and the least significant digit is at the second row.
+
+    If we have an array of operators
+    """
+    ops, columns = parse_input_p1(filepath)
+    ops = ops[::-1]
+    total = 0
+
+    for column, op in zip(list(columns)[::-1], ops[::-1]):  # (64, 16, 1512, 8719)
+        temp = [
+            tuple(int(digit) for digit in str(num)[::-1]) for num in column
+        ]  # [46, 61, 2151, 9178) -> reverses the integer.
+        temp = zip_longest(
+            *temp, fillvalue=''
+        )  # ( (4, 6, 2, 9), (6, 1, 1, 1), (5, 7), (1, 8)) -> pairs each i index in the numbers
+        numbers = [int(''.join(str(digit) for digit in num)) for num in temp]
+
+        total += reduce(OPERATORS[op], numbers)
+
+    return total
+
+
+print(grand_total_p2('advent_of_code/day_six/input.txt'))
