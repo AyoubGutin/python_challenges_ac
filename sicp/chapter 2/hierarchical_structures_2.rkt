4f81496d08db2f00f6bd56ddab44ba2ef7fc436a
Ayoub Gutin <gutinayoub@gmail.com>
2026-09-06 19:09:39 +0100

    4.9-51 exercises

--- sicp/chapter 2/hierarchical_structures_2.rkt
+++ sicp/chapter 2/hierarchical_structures_2.rkt
@@ -138,3 +138,129 @@
 (define (edge2-frame2 v)
   (cdr (cdr v)))
   
+
+; Exercise 2.48
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+
+
+; Exercixe 2.49
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) 
+         (start-segment segment))
+        ((frame-coord-map frame) 
+         (end-segment segment))))
+     segment-list)))
+
+; outline of the frame
+(define outline-painter
+  (segments->painter
+    (list
+      (make-segment (make-vect 0.0 0.0) (make-vect 1.0 0.0))
+      (make-segment (make-vect 1.0 0.0) (make-vect 1.0 1.0))
+      (make-segment (make-vect 1.0 1.0) (make-vect 0.0 1.0))
+      (make-segment (make-vect 0.0 1.0) (make-vect 0.0 0.0)))))
+
+; Draws an x
+(define x-painter
+  (segments->painter
+   (list
+    (make-segment (make-vect 0.0 0.0) (make-vect 1.0 1.0))
+    (make-segment (make-vect 0.0 1.0) (make-vect 1.0 0.0)))))
+
+; Draws a diamond
+(define diamond-painter
+  (segments->painter
+   (list
+    (make-segment (make-vect 0.0 0.5) (make-vect 0.5 1.0))
+    (make-segment (make-vect 0.5 1.0) (make-vect 1.0 0.5))
+    (make-segment (make-vect 1.0 0.5) (make-vect 0.5 0.0))
+    (make-segment (make-vect 0.5 0.0) (make-vect 0 0.5)))))
+
+
+
+; Exercise 2.50
+(define (transform-painter 
+         painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame new-origin
+                  (sub-vect (m corner1) 
+                            new-origin)
+                  (sub-vect (m corner2)
+                            new-origin)))))))
+
+; flips a painter horizontally
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+; Exercixe 2.51
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left  (transform-painter 
+                        painter1
+                        (make-vect 0.0 0.0)
+                        split-point
+                        (make-vect 0.0 1.0)))
+          (paint-right (transform-painter
+                        painter2
+                        split-point
+                        (make-vect 1.0 0.0)
+                        (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+(define (below-2 painter1 painter2)
+  (rotate-90 (beside (flip-270 painter1)
+                     (flip-270 painter2))))
+
